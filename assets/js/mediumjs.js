5f16a3ff0bec4d46310ea90d2d4f2a7bfd3234af
AlexDiGioia <digioia.alex2001@gmail.com>
2024-05-31 14:55:27 +0200

    funzione delle m

---
+++ assets/js/mediumjs.js
@@ -0,0 +1,30 @@
+const mList = document.querySelectorAll("#mRefer g"); //su html ho assegnato la classe mRefer al g padre di tutti gli altri g
+console.log(mList);
+const mListOperable=[];
+//mList[118].style.opacity="0";
+
+//let prova = mList[0].style.opacity.toString();
+
+//console.log(style.opacity);
+mList.forEach(element => {
+    let style = getComputedStyle(element);
+    if(style.opacity==1 && element.parentElement.id){
+        mListOperable.push(element);
+    }
+});
+console.log(mListOperable);
+
+const opacityInterval=setInterval(function(){
+
+    let cont=Math.floor(Math.random() * mListOperable.length)
+    let styleOp = getComputedStyle(mListOperable[cont]);
+    if(styleOp.opacity==0){
+        mListOperable[cont].style.opacity=1;
+    }
+    else if(styleOp.opacity==1){
+        mListOperable[cont].style.opacity=0;
+    }
+    
+    console.log("sas");
+    
+},100)
